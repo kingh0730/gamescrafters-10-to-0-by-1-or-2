9ddfdade7a27e57e6fa4f6ba492a46251d62da9f
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-02-03 18:28:02 -0800

    interesting

--- src/game_tic_tac_toe.rs
+++ src/game_tic_tac_toe.rs
@@ -2,10 +2,19 @@ use crate::{GameResult, Move, Position, PrimitiveValue};
 
 const LENGTH: usize = 3;
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 enum TicTacToePlayer {
     X,
     O,
+}
+
+impl TicTacToePlayer {
+    fn next_player(&self) -> TicTacToePlayer {
+        match self {
+            TicTacToePlayer::X => TicTacToePlayer::O,
+            TicTacToePlayer::O => TicTacToePlayer::X,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -17,9 +26,9 @@ enum TicTacToePrimitiveValue {
 }
 
 #[derive(Debug)]
-enum TicTacToeMove {
-    Take1,
-    Take2,
+struct TicTacToeMove {
+    x: u32,
+    y: u32,
 }
 
 #[derive(Debug, PartialEq, Eq, Hash)]
@@ -29,3 +38,36 @@ struct TicTacToePosition {
 }
 
 impl Move for TicTacToeMove {}
+
+impl PrimitiveValue for TicTacToePrimitiveValue {
+    fn result(&self) -> Option<GameResult> {
+        match self {
+            TicTacToePrimitiveValue::Win => Some(GameResult::Win),
+            TicTacToePrimitiveValue::Lose => Some(GameResult::Lose),
+            TicTacToePrimitiveValue::Tie => Some(GameResult::Tie),
+            TicTacToePrimitiveValue::NotPrimitive => None,
+        }
+    }
+}
+
+impl Position for TicTacToePosition {
+    type GameMove = TicTacToeMove;
+    type GamePrimitiveValue = TicTacToePrimitiveValue;
+
+    fn do_move(&self, mov: TicTacToeMove) -> TicTacToePosition {
+        let board = self.board.clone();
+
+        TicTacToePosition {
+            board,
+            player: self.player.next_player(),
+        }
+    }
+
+    fn generate_moves(&self) -> Vec<TicTacToeMove> {
+        vec![]
+    }
+
+    fn primitive_value(&self) -> TicTacToePrimitiveValue {
+        TicTacToePrimitiveValue::NotPrimitive
+    }
+}
