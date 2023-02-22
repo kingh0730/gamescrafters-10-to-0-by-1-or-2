4852321bad5c669c302ef47b0b3c5fb5b6fdd0b5
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-02-22 12:04:17 -0800

    cool

--- src/games/tic_tac_toe_non_sq.rs
+++ src/games/tic_tac_toe_non_sq.rs
@@ -1,1 +1,31 @@
 mod position;
+
+use crate::solver::{PlayerMove, PrimitiveValue};
+
+const WIDTH: usize = 4;
+const HEIGHT: usize = 3;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum TicTacToeNonSqPrimitiveValue {
+    _Win,
+    Lose,
+    Tie,
+    NotPrimitive,
+}
+
+impl PrimitiveValue for TicTacToeNonSqPrimitiveValue {
+    fn is_primitive(&self) -> bool {
+        match self {
+            TicTacToeNonSqPrimitiveValue::NotPrimitive => false,
+            _ => true,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct TicTacToeNonSqMove {
+    x: usize,
+    y: usize,
+}
+
+impl PlayerMove for TicTacToeNonSqMove {}
